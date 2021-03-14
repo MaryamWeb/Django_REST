3706acc78c3e5e52a19d6c002a5b8b889795bd7e
unknown <maryam_lfc@hotmail.com>
2021-03-14 22:12:46 +0300

    Create ViewSets

--- authProject/authApp/views.py
+++ authProject/authApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from authApp.models import Hotel,Guest,Reservation
+from authApp.serializers import HotelSerializer,GuestSerializer,ReservationSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class HotelViewSet(viewsets.ModelViewSet):
+    queryset=Hotel.objects.all()
+    serializer_class=HotelSerializer
+    
+class GuestViewSet(viewsets.ModelViewSet):
+    queryset=Guest.objects.all()
+    serializer_class=GuestSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset=Reservation.objects.all()
+    serializer_class=ReservationSerializer
