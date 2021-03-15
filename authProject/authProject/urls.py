45512095bb4e0724a90f88fd547071925986b268
unknown <maryam_lfc@hotmail.com>
2021-03-15 20:00:45 +0300

    Configure the router

--- authProject/authProject/urls.py
+++ authProject/authProject/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from authApp import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hotels',views.HotelViewSet)
+router.register('guests',views.GuestViewSet)
+router.register('reservations',views.ReservationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(router.urls))
 ]
