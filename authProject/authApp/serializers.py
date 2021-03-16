bbcac3dbec501ff502db98e53587487caaab5a6b
unknown <maryam_lfc@hotmail.com>
2021-03-16 21:04:18 +0300

    Add custom validation

--- authProject/authApp/serializers.py
+++ authProject/authApp/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from authApp.models import Hotel,Guest,Reservation
+import re
  
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +11,11 @@ class GuestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Guest
         fields='__all__'
+
+    def validate_firstName(self,firstName):
+        if(re.match("^[a-zA-Z0-9]*$", firstName)==None):  
+            raise serializers.ValidationError("Invalid name. Make sure name only contains letters and numbers")
+        return firstName
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
