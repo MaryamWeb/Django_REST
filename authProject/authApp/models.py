b76e00f6ca2310d7d59bbe318d07b39bdc144209
unknown <maryam_lfc@hotmail.com>
2021-03-17 20:05:45 +0300

    Automate Token creation

--- authProject/authApp/models.py
+++ authProject/authApp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings 
 
 # Create your models here.
 class Hotel(models.Model):
@@ -28,3 +32,8 @@ class Guest(models.Model):
 class Reservation(models.Model):
     hotel = models.ForeignKey(Hotel,on_delete=models.CASCADE)
     guest = models.OneToOneField(Guest,on_delete=models.CASCADE)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def createAuthToken(sender, instance, created, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
